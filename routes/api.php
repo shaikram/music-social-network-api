75a78f5c16c7e9aadd30cdf7b618a1e100cd3864
shaikram <shaikram02@gmail.com>
2023-02-04 14:27:57 -0800

    Initial Commit

---
+++ routes/api.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('test', function () {
+    return "Hello World";
+});
+
+Route::post('register', [\App\Http\Controllers\API\AuthController::class, 'register']);
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('inside-mware', function () {
+        return response()->json('success', 200);
+    });
+
+    // Route::get('users/{id}', [\App\Http\Controllers\API\UserController::class, 'show']);
+    // Route::put('users/{id}', [\App\Http\Controllers\API\UserController::class, 'update']);
+
+    // Route::get('youtube/{user_id}', [\App\Http\Controllers\API\YoutubeController::class, 'show']);
+    // Route::post('youtube', [\App\Http\Controllers\API\YoutubeController::class, 'store']);
+    // Route::delete('youtube/{id}', [\App\Http\Controllers\API\YoutubeController::class, 'destroy']);
+
+    // Route::post('songs', [\App\Http\Controllers\API\SongController::class, 'store']);
+    // Route::delete('songs/{id}/{user_id}', [\App\Http\Controllers\API\SongController::class, 'destroy']);
+
+    // Route::get('user/{user_id}/songs', [\App\Http\Controllers\API\SongsByUserController::class, 'index']);
+
+    // Route::get('posts', [\App\Http\Controllers\API\PostController::class, 'index']);
+    // Route::get('posts/{id}', [\App\Http\Controllers\API\PostController::class, 'show']);
+    // Route::post('posts', [\App\Http\Controllers\API\PostController::class, 'store']);
+    // Route::put('posts/{id}', [\App\Http\Controllers\API\PostController::class, 'update']);
+    // Route::delete('posts/{id}', [\App\Http\Controllers\API\PostController::class, 'destroy']);
+
+    // Route::get('user/{user_id}/posts', [\App\Http\Controllers\API\PostsByUserController::class, 'show']);
+
+    // Route::post('logout', [\App\Http\Controllers\API\AuthController::class, 'logout']);
+
+});
